17f02169254d882647a33769537b0e9180b5bf50
azeemnitw <azeemmohd.azeem752@gmail.com>
2024-10-13 18:16:35 +0530

    2nd commit

--- Introduction/src/TravelBooking.java
+++ Introduction/src/TravelBooking.java
@@ -2,8 +2,10 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.Select;
+ 
 public class TravelBooking {
 
 	public static void main(String[] args) {
@@ -11,18 +13,25 @@ public class TravelBooking {
 		WebDriver dr=new ChromeDriver();
 		dr.manage().window().maximize();
 		dr.get("https://www.dummyticket.com/");
-		dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		dr.findElement(By.xpath("//a[text()='Buy Ticket']")).click();
-		dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		dr.findElement(By.xpath("//input[@id='travname']")).sendKeys("rohit");
 		dr.findElement(By.xpath("//input[@id='travlastname']")).sendKeys("kumar");
 		
 		dr.findElement(By.id("dob")).click();
 		
-		while(!dr.findElement(By.xpath("//select[@class='ui-datepicker-month']")).getText().contains("Jan")) {
-		    dr.findElement(By.cssSelector("//a[@data-handler='prev']")).click();
-		}
-
+		WebElement ddown=dr.findElement(By.xpath("//select[@class='ui-datepicker-month']"));
+		Select sel=new Select(ddown);
+		sel.selectByValue("5");
+		WebElement ddown1=dr.findElement(By.xpath("//select[@class='ui-datepicker-year']"));
+		Select sel1=new Select(ddown1);
+		sel1.selectByValue("1995");
+		dr.findElement(By.xpath("//a[@data-date='15']")).click();
+		
+		//dr.close();		
+		
+		
 	}
 
 }
