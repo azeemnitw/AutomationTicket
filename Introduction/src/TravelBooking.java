67cca3278abd5f2d9a5da5e5e0a4089c7d0371d0
azeemnitw <azeemmohd.azeem752@gmail.com>
2024-10-15 16:24:16 +0530

    4th commit

--- Introduction/src/TravelBooking.java
+++ Introduction/src/TravelBooking.java
@@ -3,6 +3,7 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -46,6 +47,53 @@ public class TravelBooking {
 		}
 		dr.findElement(By.xpath("//input[@id='fromcity']")).sendKeys("Delhi");
 		dr.findElement(By.xpath("//input[@id='tocity']")).sendKeys("Bengaluru");
+		
+		//here I am selecting departure date
+		dr.findElement(By.id("departon")).click();
+		
+		WebElement ddown2=dr.findElement(By.xpath("//select[@class='ui-datepicker-year']"));
+		Select sel2=new Select(ddown2);
+		sel2.selectByValue("2025");
+		
+		WebElement ddown3=dr.findElement(By.xpath("//select[@class='ui-datepicker-month']"));
+		Select sel3=new Select(ddown3);
+		sel3.selectByValue("0");
+		dr.findElement(By.xpath("//a[@data-date='8']")).click();
+		//selection ended here
+		
+		//here i am selecting return date
+        dr.findElement(By.id("returndate")).click();
+		
+		WebElement ddown4=dr.findElement(By.xpath("//select[@class='ui-datepicker-year']"));
+		Select sel4=new Select(ddown4);
+		sel4.selectByValue("2025");
+		
+		WebElement ddown5=dr.findElement(By.xpath("//select[@class='ui-datepicker-month']"));
+		Select sel5=new Select(ddown5);
+		sel5.selectByValue("0");
+		dr.findElement(By.xpath("//a[@data-date='13']")).click();
+		//return date selected
+		
+		//selecting message on both email and whatsapp
+		List<WebElement>ele=dr.findElements(By.name("deliverymethod"));
+		for(WebElement e:ele) {
+			if(e.getAttribute("value").equalsIgnoreCase("3")) e.click();
+		}
+		
+		dr.findElement(By.id("billing_email")).sendKeys("prashant@gmail.com");
+		dr.findElement(By.id("billing_phone")).sendKeys("9966885577");
+		dr.findElement(By.id("billing_address_1")).sendKeys("100 new area");
+		dr.findElement(By.id("billing_address_2")).sendKeys("anand vihar");
+		dr.findElement(By.id("billing_city")).sendKeys("Delhi");
+		
+		//selecting state of customer
+		dr.findElement(By.id("select2-billing_state-container")).click();
+		dr.findElement(By.xpath("//input[@role='combobox']")).sendKeys("Delhi");
+		dr.findElement(By.xpath("//input[@role='combobox']")).sendKeys(Keys.ARROW_DOWN);
+		dr.findElement(By.xpath("//input[@role='combobox']")).sendKeys(Keys.ENTER);
+		dr.findElement(By.id("billing_postcode")).sendKeys("110092");
+		dr.findElement(By.id("place_order")).click();
+		
 		
 	}
 
